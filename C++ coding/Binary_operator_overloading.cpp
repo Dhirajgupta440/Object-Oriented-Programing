5fca1522d0c5008516fbc44635aa02d0c4f26488
Dhiraj Gupta <157223950+Dhirajgupta440@users.noreply.github.com>
2024-06-24 00:58:52 +0530

    Add files via upload

---
+++ C++ coding/Binary_operator_overloading.cpp
@@ -0,0 +1,31 @@
+//Binary operator overloading using class 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class demo{
+    int a,b;
+    public:
+    demo(int x,int y){
+    a=x;
+    b=y;
+    }
+    void show(){
+        cout<<"A="<<a<<" "<<"B="<<b<<endl;
+    }
+     demo operator +(demo obj){
+        demo temp(0,0);
+        temp.a=a+obj.a;
+        temp.b=b+obj.b;
+        return temp;
+     }
+    
+};
+
+
+int main(){
+    demo ob(10,20),ob1(30,40),ob2(0,0);
+    ob2=ob+ob1;
+    ob2.show();
+
+    
+}
